1aad88b293f5224b62f7963daa44a8c12adb49b3
ankitjhagithub21 <ajha45075@gmail.com>
2025-04-10 20:26:03 +0530

    design admin home page

--- frontend/src/admin/AdminHome.jsx
+++ frontend/src/admin/AdminHome.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import {useSelector} from 'react-redux'
 
 const AdminHome = () => {
+  const {user} = useSelector(state=>state.auth)
   return (
-    <div>AdminHome</div>
+    <div className='flex flex-col items-center gap-4'>
+      <img src={user.profile.profilePhoto} alt="profile" className='w-24 h-24 border border-gray-400 rounded-full object-cover object-center' />
+      <h2 className='text-3xl font-medium'>Welcome {user.fullName}</h2>
+
+    </div>
   )
 }
 
